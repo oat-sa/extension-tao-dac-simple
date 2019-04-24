d6918a2d5d3c41798c8420cb58c07e399ebe1086
Martijn Swinkels <martijn@taotesting.com>
2019-04-24 14:03:15 +0200

    Updated CSRF failure reporting

--- controller/AdminAccessController.php
+++ controller/AdminAccessController.php
@@ -108,7 +108,7 @@ class AdminAccessController extends \tao_actions_CommonModule
         try {
             $this->validateCsrf();
         } catch (\common_exception_Unauthorized $e) {
-            $this->response = $this->getPsrResponse()->withStatus('412', _('CSRF validation failed'));
+            $this->response = $this->getPsrResponse()->withStatus('412', __('Unable to process your request'));
             return;
         }
 
