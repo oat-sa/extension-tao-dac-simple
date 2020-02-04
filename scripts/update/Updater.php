17cce43f90833b798b0e369fcb162acf43b956f3
Razhkou <yuryr@taotesting.com>
2020-02-04 10:37:50 +0300

    Version bump

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -138,6 +138,6 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('2.7.0');
         }
 
-        $this->skip('2.7.0', '5.1.1');
+        $this->skip('2.7.0', '6.0.0');
     }
 }
