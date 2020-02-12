e1a1cbfa19c6ec73a901eb403109beb517c9432e
Sergii <sergii@taotesting.com>
2020-02-12 11:02:05 +0100

    Event added

---
+++ model/event/DacAffectedUsersEvent.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2020 (original work) Open Assessment Technologies SA;
+ *
+ */
+
+namespace oat\taoDacSimple\model\event;
+
+use oat\oatbox\event\Event;
+
+class DacAffectedUsersEvent implements Event
+{
+    /**
+     * @var string[]
+     */
+    private $listOfNewUserIds;
+    /**
+     * @var string[]
+     */
+    private $listOfRemovedUserIds;
+
+    public function __construct(array $listOfNewUserIds, array $listOfRemovedUserIds)
+    {
+        $this->listOfNewUserIds = $listOfNewUserIds;
+        $this->listOfRemovedUserIds = $listOfRemovedUserIds;
+    }
+
+    public function getName(): string
+    {
+        return __CLASS__;
+    }
+
+    public function getListOfNewUserIds(): array
+    {
+        return $this->listOfNewUserIds;
+    }
+
+    public function getListOfRemovedUserIds(): array
+    {
+        return $this->listOfRemovedUserIds;
+    }
+}
