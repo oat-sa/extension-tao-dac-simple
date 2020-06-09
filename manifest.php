24795fe98b4181740b3e33f43836ed1a7d1a9715
gitromba <mihaly.hajdu@taotesting.com>
2020-06-09 19:47:48 +0200

    chore: bump manifest version

--- manifest.php
+++ manifest.php
@@ -30,7 +30,7 @@ return [
     'label' => 'extension-tao-dac-simple',
     'description' => 'extension that allows admin to give access to some resources to other people',
     'license' => 'GPL-2.0',
-    'version' => '6.6.1',
+    'version' => '6.7.0',
     'author' => 'Open Assessment Technologies SA',
     'requires' => [
        'taoBackOffice' => '>=3.0.0',
