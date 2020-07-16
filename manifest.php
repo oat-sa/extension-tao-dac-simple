5b7e674d47e98b94171baf9c68074068a8f05096
Luis Nascimento <luis.nascimento@taotesting.com>
2020-07-16 16:34:49 +0200

    TAO-10365 Try to require a specific version from generis for now

--- manifest.php
+++ manifest.php
@@ -34,7 +34,7 @@ return [
     'author' => 'Open Assessment Technologies SA',
     'requires' => [
        'taoBackOffice' => '>=3.0.0',
-       'generis' => '>=12.15.0',
+       'generis' => 'dev-feature/TAO-10365-apply-access-control-to-queries',
        'tao' => '>=40.9.0'
     ],
     // for compatibility
