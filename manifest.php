125562ecb4b211b4f90b77372c1cc58cf3b51aee
Aleh Hutnikau <hutnikau@1pt.com>
2021-02-17 16:45:41 +0300

    chore!: move dependencies to composer.json

--- manifest.php
+++ manifest.php
@@ -30,15 +30,7 @@ return [
     'label' => 'extension-tao-dac-simple',
     'description' => 'extension that allows admin to give access to some resources to other people',
     'license' => 'GPL-2.0',
-    'version' => '6.8.3',
     'author' => 'Open Assessment Technologies SA',
-    'requires' => [
-       'taoBackOffice' => '>=3.0.0',
-       'generis' => '>=12.15.0',
-       'tao' => '>=40.9.0'
-    ],
-    // for compatibility
-    'dependencies' => ['tao', 'taoItems'],
     'managementRole' => 'http://www.tao.lu/Ontologies/generis.rdf#taoDacSimpleManager',
     'acl' => [
         ['grant', 'http://www.tao.lu/Ontologies/generis.rdf#taoDacSimpleManager', ['ext' => 'taoDacSimple']],
