f78a9e6801d6af0f266b98608f104d14893257ac
Gabriel Felipe Soares <gabriel.felipe.soares@taotesting.com>
2021-04-15 15:05:33 +0200

    chore: remove unused import

--- model/RolePrivilegeRetriever.php
+++ model/RolePrivilegeRetriever.php
@@ -24,7 +24,6 @@ declare(strict_types=1);
 namespace oat\taoDacSimple\model;
 
 use oat\oatbox\service\ConfigurableService;
-use oat\taoDacSimple\model\DataBaseAccess;
 
 class RolePrivilegeRetriever extends ConfigurableService
 {
